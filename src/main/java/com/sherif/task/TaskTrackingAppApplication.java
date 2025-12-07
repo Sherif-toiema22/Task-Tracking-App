c087d6c8118783a20b1cc57d0916fbfd91380919
Sherif-toiema22 <sherif.toiema22@gmail.com>
2025-12-07 23:12:34 +0200

    changing scope from runtime to test

---
+++ src/main/java/com/sherif/task/TaskTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.sherif.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sherif/task/TaskTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherif.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
